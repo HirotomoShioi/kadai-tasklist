846d2af13d7f976768a93310bd38334802a56841
H.S <shio3124@gmail.com>
2018-08-27 12:32:16 +0000

    add colum

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -46,11 +46,13 @@ class TasksController extends Controller
     {
          $this->validate($request, [
         'content' => 'required|max:191',
+        'status' => 'required|max:10',
         ]);    
         
         
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status; 
         $task->save();
 
         return redirect('/');
@@ -98,10 +100,13 @@ class TasksController extends Controller
     {
         $this->validate($request, [
             'content' => 'required|max:191',
+            'status' => 'required|max:10',
+            
         ]);
         
         $task = Task::find($id);
         $task->content = $request->content;
+        $task->title = $request->status;
         $task->save();
 
         return redirect('/');
